d8da50acd72eda32c29c375207d1fead8fa454a8
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-08-08 14:57:51 -0700

    added on-the-fly command-line scan feature

--- credentialdigger/__main__.py
+++ credentialdigger/__main__.py
@@ -3,19 +3,19 @@ import logging
 if __name__ == "__main__":
     import plac
     import sys
-    from credentialdigger import download
+    from credentialdigger import download, scan
 
-    commands = {
-        'download': download
-    }
+    commands = {'download': download, 'scan': scan}
     available_comamnds = f'{", ".join(commands)}'
     if len(sys.argv) == 1:
-        logging.info(f'Available commands: {available_comamnds}')
+        logging.error(
+            f'You have not called any command.\nAvailable commands: {available_comamnds}'
+        )
+        exit()
     command = sys.argv.pop(1)
     sys.argv[0] = f'credentialdigger {command}'
     if command in commands:
         plac.call(commands[command], sys.argv[1:])
     else:
-        logging.error(
-            f'Unknown command: {command}\n'
-            f'Available commands: {available_comamnds}')
+        logging.error(f'Unknown command: {command}\n'
+                      f'Available commands: {available_comamnds}')
