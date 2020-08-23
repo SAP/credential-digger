a79767867e369afa1e00ada870dfada8af4c9f11
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-08-22 17:54:57 -0700

    Errros indicators & JS code enhancements

--- ui/res/js/repos.js
+++ ui/res/js/repos.js
@@ -1,63 +1,107 @@
-// add search action listener
-document.getElementById('search').addEventListener('input', function() {
-  search(this.value);
-});
+/**
+ * URL: The URL is invalid.
+ * CATEGORY: The user must select one category.
+ */
+const Errors = Object.freeze({ "URL": 1, "CATEGORY": 2 });
 
-// add repo pop up
-// add action listener to add repo button
-document.getElementById('addRepo').addEventListener('click', function() {
-  // show popup
-  document.getElementById('addRepoModal').style.display = 'block';
-});
+window.onload = function () {
+  // add search action listener
+  document.getElementById('search').addEventListener('input', function () {
+    search(this.value);
+  });
 
-// add action listener for window (clicking anywhere)
-window.addEventListener('click', function(event) {
-  // if user clicked in the modal area (area around the popup) hide popup
-  if (event.target == document.getElementById('addRepoModal')) {
-    closeAddRepo();
-  }
-});
+  // add repo pop up
+  // add action listener to add repo button
+  document.getElementById('addRepo').addEventListener('click', function () {
+    // show popup
+    document.getElementById('addRepoModal').style.display = 'block';
+  });
 
-// add action listener to close add repo popup
-document.getElementById('cancelAddRepo').addEventListener('click', function() {
-  // close popup
-  closeAddRepo();
-});
+  // add action listener for window (clicking anywhere)
+  window.addEventListener('click', function (event) {
+    // if user clicked in the modal area (area around the popup) hide popup
+    if (event.target == document.getElementById('addRepoModal')) {
+      closeAddRepo();
+    }
+  });
 
-// add action listener to start repo scan
-document.getElementById('startRepoScan').addEventListener('click', function() {
-  // close popup
-  document.getElementById('addRepoModal').style.display = 'none';
-  // show loading popup
-});
+  // add action listener to close add repo popup
+  document.getElementById('cancelAddRepo').addEventListener('click', closeAddRepo());
 
-// add action listener to repo url input
-document.getElementById('repoLinkInput').addEventListener('input', function() {
-  // check if form is correctly filled
-  checkFormFilled();
-});
+  // add action listener to start repo scan
+  document.getElementById('startRepoScan').addEventListener('click', function () {
+    // close popup
+    document.getElementById('addRepoModal').style.display = 'none';
+    // show loading popup
+  });
 
-// add action listener to repo scan config selector
-document.getElementById('cbAllRules').addEventListener('change', function() {
-  // check if forenameFolderModalrm is correctly filled
-  checkFormFilled();
-});
+  document.getElementById('ruleSelector').addEventListener('change', checkFormFilled);
 
-// check if form is filled correctly and handle change
+  // add action listener to repo url input
+  document.getElementById('repoLinkInput').addEventListener('input', checkFormFilled);
+
+  // add action listener to repo scan config selector
+  document.getElementById('cbAllRules').addEventListener('change', checkFormFilled);
+};
+
+/**
+ * Check if form is filled correctly and handle change
+ */
+
 function checkFormFilled() {
   // get post repo scan button and form values
-  let postAddRepoButton = document.getElementById('startRepoScan');
-  let repoLink = document.getElementById('repoLinkInput').value;
+  let cBox = document.getElementById('cbAllRules');
+  let rulesList = document.getElementById('ruleSelector');
+  let repoLinkContainer = document.getElementById('repoLinkInput');
+  let repoLink = repoLinkContainer.value;
   // check if repo link is a valid url
   let urlValid = repoLink.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   // enable submit button if url is valid
-  if (urlValid) {
+  console.log(urlValid);
+
+  if (urlValid || cBox.checked || rulesList.selectedIndex != -1) {
+    editForm(true);
+  }
+  if (urlValid == null) {
+    editForm(false, Errors.URL, 'The URL you have provided is invalid.');
+    return;
+  }
+  if (cBox.checked == true || rulesList.selectedIndex == -1) {
+    editForm(false, Errors.CATEGORY, 'Please select a category first.');
+    return;
+  }
+
+}
+
+/**
+ * A function to enable/disable the scan button and highlight alarming sections.
+ * @param {boolean} enable 
+ * True: Enables the button | False: Disables the button
+ * @param {string} tooltip 
+ * (Optional) Shows an error message when the mouse hovers on the disabled button
+ */
+function editForm(enable, err, tooltip = '') {
+  let postAddRepoButton = document.getElementById('startRepoScan');
+  let repoLinkContainer = document.getElementById('repoLinkInput');
+  let rulesList = document.getElementById('ruleSelector');
+  if (enable) {
     postAddRepoButton.disabled = false;
+    postAddRepoButton.title = tooltip;
     postAddRepoButton.classList.remove('disabledButton');
-    // else disable submit
+    repoLinkContainer.style.border = '1px solid black';
+    rulesList.style.border = '1px solid black';
   } else {
     postAddRepoButton.disabled = true;
+    postAddRepoButton.title = tooltip;
     postAddRepoButton.classList.add('disabledButton');
+    switch (err) {
+      case Errors.URL:
+        repoLinkContainer.style.border = '2px solid red';
+        break;
+      case Errors.CATEGORY:
+        rulesList.style.border = '2px solid red';
+        break;
+    }
   }
 }
 
@@ -71,5 +115,5 @@ function closeAddRepo() {
   document.getElementById('cbAllRules').checked = false;
   document.getElementById('cbSnippetModel').checked = false;
   document.getElementById('cbAllRules').checked = false;
-  checkFormFilled();
 }
+
