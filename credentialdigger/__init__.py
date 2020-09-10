6a905c62a6815b744f77cec82dbf0d2601e57406
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-09-10 12:09:12 -0700

    add_rules support

--- credentialdigger/__init__.py
+++ credentialdigger/__init__.py
@@ -1,5 +1,6 @@
 from .client import Client
 from .client_postgres import PgClient
 from .client_sqlite import SqliteClient
+from .add_rules import add_rules
 from .download import download
 from .scan import scan
