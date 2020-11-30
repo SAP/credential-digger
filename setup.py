bf1379d6a5fd9eba70279f2003f815a88f55e8fd
Marco Rosa <marco.rosa@sap.com>
2020-11-30 16:31:31 +0100

    fix git token usage in scan_user and scan_wiki

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 setuptools.setup(
     name='credentialdigger',
-    version='2.1.2',
+    version='2.1.3',
     author='SAP SE',
     maintainer='Marco Rosa, Slim Trabelsi',
     maintainer_email='marco.rosa@sap.com, slim.trabelsi@sap.com',
