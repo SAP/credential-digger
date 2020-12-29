d9e05e6b52beb9c7b51b33f334eedebf3d294cc2
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2020-12-29 16:17:13 +0100

    Add delete repo to repos view

--- ui/res/js/discoveries.js
+++ ui/res/js/discoveries.js
@@ -155,32 +155,6 @@ function initDiscoveriesDataTable() {
   });
 }
 
-function initButtonGroup() {
-  // Only have one button group active at a time
-  var activeBtnGroup = null;
-
-  // Toggle button dropdown when clicking on the opener
-  $(document).on('click', '.btn-group .dropdown-opener, .btn-group .dropdown, .btn-group.active .default-btn', function () {
-    const parent = this.closest('.btn-group');
-    const dropdownOpen = parent.classList.contains('active');
-    if (dropdownOpen) {
-      parent.classList.remove('active');
-      activeBtnGroup = null;
-    } else {
-      if (activeBtnGroup) activeBtnGroup.classList.remove('active');
-      parent.classList.add('active');
-      activeBtnGroup = parent;
-    }
-  });
-
-  // Close button dropdown when clicking outside of the button
-  $(document).on('click', function (e) {
-    if (!activeBtnGroup || activeBtnGroup.contains(e.target)) return;
-    activeBtnGroup.classList.remove('active');
-    activeBtnGroup = null;
-  });
-}
-
 function initUpdateDiscoveries() {
   const repoUrl = document.querySelector('#repo-url').innerText;
 
