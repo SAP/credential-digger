c1fe10d366ad377839ade9e8a8b9e16ee2f1d66e
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2021-03-05 15:45:57 +0100

    Remove timestamp checks in file scanner

--- credentialdigger/tests/test_file_scanner.py
+++ credentialdigger/tests/test_file_scanner.py
@@ -1,6 +1,5 @@
 import os
 import unittest
-from datetime import datetime, timezone
 
 from credentialdigger.scanners.file_scanner import FileScanner
 from parameterized import param, parameterized
@@ -23,14 +22,14 @@ class TestFileScanner(unittest.TestCase):
         self.assertEqual(len(discoveries), 3)
 
     @parameterized.expand(
-        ["", "/nonexistentdirectory/", "https://url", "not_a_path"])
+        ["/nonexistentdirectory/", "https://url", "not_a_path"])
     def test_scan_dir_not_found(self, dir_path):
         """ Test scan failure with nonexistent directory paths """
         with self.assertRaises(FileNotFoundError):
             self.file_scanner.scan(dir_path)
 
     @parameterized.expand([
-        param(file_path='file_a.py', expected_discoveries=[2, 4]),
+        param(file_path='file_a.py', expected_discoveries=[2, 4, 5]),
         param(file_path='file_c.png', expected_discoveries=[])
     ])
     def test_scan_file(self, file_path, expected_discoveries):
@@ -40,37 +39,9 @@ class TestFileScanner(unittest.TestCase):
         number of the discoveries in the file.
         """
         discoveries = self.file_scanner.scan_file(
-            os.path.join(self.root_test_path, file_path))
+            os.path.join(self.root_test_path, file_path), self.root_test_path)
         d_lines = [d["line_number"] for d in discoveries]
         self.assertCountEqual(d_lines, expected_discoveries)
-
-    @parameterized.expand([
-        # Since timestamp is 0, no file should be pruned
-        param(timestamp=0,
-              expected_dirs=["subdir_A", "subdir_AB"],
-              expected_files=["file_a.py", "file_b.txt",
-                              "file_Aa.yml", "file_ABa.txt"]),
-        # Since timestamp is now, all files should be pruned.
-        # NOTE: the mtime of a directory does not change if the content of
-        # a file inside of it changes. Therefore no folders should be pruned.
-        param(timestamp=int(datetime.now(timezone.utc).timestamp()),
-              expected_dirs=["subdir_A", "subdir_AB"],
-              expected_files=[]),
-    ])
-    def test_prune_timestamp(self, timestamp, expected_dirs, expected_files):
-        """ Test prune with respect to the `since_timestamp` argument """
-        initial_depth = self.root_test_path.count(os.path.sep)
-        all_dirs = []
-        all_files = []
-
-        for root, dirs, files in os.walk(self.root_test_path):
-            self.file_scanner._prune(root, dirs, files, initial_depth,
-                                     since_timestamp=timestamp)
-            all_dirs.extend(dirs)
-            all_files.extend(files)
-
-        set(expected_dirs).issubset(set(all_dirs))
-        set(expected_files).issubset(set(all_files))
 
     @parameterized.expand([
         param(max_depth=0,
