739662ffee6680d55fcd4d0e4db08f114c4b5e94
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2021-03-17 15:40:36 +0100

    Fix bug where discoveries in latest commit would not be detected Add tests for git_scanner

--- tests/test_git_scanner.py
+++ tests/test_git_scanner.py
@@ -1,21 +1,33 @@
-import os
 import shutil
+import tempfile
 import unittest
+from datetime import datetime, timezone
 
 from credentialdigger.scanners.git_scanner import GitScanner
 from git import GitCommandError, InvalidGitRepositoryError
+from git import Repo as GitRepo
 from parameterized import param, parameterized
 
 
 class TestGitScanner(unittest.TestCase):
-    repo_path = 'https://github.com/fabiosangregorio/credential-digger-tests'
 
     @classmethod
     def setUpClass(cls):
-        """ Instantiate the scanner with only the password-related rules """
+        # Instantiate the scanner with only the password-related rules
         rules = [{'id': 9, 'regex': 'sshpass|password|pwd|passwd|pass',
                   'category': 'password', 'description': 'password keywords'}]
         cls.git_scanner = GitScanner(rules)
+
+        # Clone the test repository from Github
+        repo_url = 'https://github.com/fabiosangregorio/credential-digger-tests'
+        cls.tmp_path = tempfile.mkdtemp()
+        GitRepo.clone_from(repo_url, cls.tmp_path)
+        cls.repo = GitRepo(cls.tmp_path)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Delete repo folder
+        shutil.rmtree(cls.tmp_path)
 
     @parameterized.expand([
         "https://:@github.com/SAP/inexistent-repo",
@@ -36,8 +48,35 @@ class TestGitScanner(unittest.TestCase):
         # Inexistent path
         with self.assertRaises(FileNotFoundError):
             self.git_scanner.get_git_repo('./inexistent_path', local_repo=True)
-
         # Non-repo path
         with self.assertRaises(InvalidGitRepositoryError):
             self.git_scanner.get_git_repo(
                 './credentialdigger', local_repo=True)
+
+    def test_scan_since_timestamp_now(self):
+        """ Test that there are no new discoveries if scanning from now """
+        timestamp = int(datetime.now(timezone.utc).timestamp())
+        discoveries = self.git_scanner._scan(
+            self.repo, max_depth=1000000, since_timestamp=timestamp)
+        self.assertEqual(len(discoveries), 0)
+
+    def test_scan_since_timestamp_zero(self):
+        """ Test discoveries count since beginning """
+        discoveries = self.git_scanner._scan(
+            self.repo, max_depth=1000000, since_timestamp=0)
+        self.assertGreaterEqual(len(discoveries), 8)
+
+    def test_scan_since_after_first_commit(self):
+        """ Test only discoveries inserted after first commit """
+        first_commit_hexsha = "ee06dc139ab360080734b3ee595e6f2627094968"
+        first_commit_timestamp = 1615976544
+        discoveries = self.git_scanner._scan(
+            self.repo,
+            max_depth=1000000,
+            since_timestamp=first_commit_timestamp + 1)
+
+        # We know that at least one discovery was inserted after first commit
+        self.assertGreaterEqual(len(discoveries), 1)
+
+        for d in discoveries:
+            self.assertNotEqual(d['commit_id'], first_commit_hexsha)
