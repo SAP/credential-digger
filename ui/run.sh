1df3f2d2b1a32cc0117a1c9bb0ca33ebd917b9e2
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-04-09 05:52:18 -0700

    fix path typo

--- ui/run.sh
+++ ui/run.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Download models..."
 
-if [[ -z "${path_model}" ]]; then
+if [ -z "${path_model}" ]; then
     echo "Path model not provided"
 else
     echo "Running with path_model=$path_model"
@@ -12,9 +12,29 @@ fi
 if [[ -z "${snippet_model}" ]]; then
     echo "Snippet model not provided"
 else
-    echo "Running with snippet_model=$snippet_model"
+    echo "Running with snipper_model=$snippet_model"
     python -m credentialdigger download snippet_model
 fi
 
+# Set HTTPS flag to True
+HTTPS=true
+
+if [[ -z "${SSL_certificate}" ]]; then
+    # No certificate
+    HTTPS=false
+
+fi
+
+if [[ -z "${SSL_private_key}" ]]; then
+    # No private key
+    HTTPS=false
+fi
+
 echo "Starting server..."
-python /credential-digger-ui/server.py
+if [ "$HTTPS" = true ]; then
+    echo "🔐 HTTPS will be used..."
+    gunicorn -b 0.0.0.0:5000 wsgi:app --certfile /credential-digger-ui$SSL_certificate --keyfile /credential-digger-ui$SSL_private_key
+else
+    echo "🔓 HTTPS will not be used... (HTTP only)"
+    gunicorn -b 0.0.0.0:5000 wsgi:app
+fi
