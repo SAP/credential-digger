052cfdba939111d2a7f7f88ac5d3b264c7b7003d
Marco Rosa <marco.rosa@sap.com>
2021-05-11 15:45:15 +0200

    update pass rule

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 setuptools.setup(
     name='credentialdigger',
-    version='3.1.1',
+    version='3.1.2',
     author='SAP SE',
     maintainer='Marco Rosa, Slim Trabelsi',
     maintainer_email='marco.rosa@sap.com, slim.trabelsi@sap.com',
