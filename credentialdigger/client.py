5a57a32edcf8174f91fea4163e5b5be4f4d3e625
melisandequentzel <melisande.quentzel@sap.com>
2021-05-19 15:26:08 +0000

    add similarity model

--- credentialdigger/client.py
+++ credentialdigger/client.py
@@ -12,6 +12,7 @@ from .generator import ExtractorGenerator
 from .models.model_manager import ModelManager
 from .scanners.file_scanner import FileScanner
 from .scanners.git_scanner import GitScanner
+import .snippet_similarity.py
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
