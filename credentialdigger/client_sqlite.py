446bb8f501555fb203c883f4896a2089f3e15135
melisandequentzel <melisande.quentzel@gmail.com>
2021-05-20 14:05:52 +0000

    fix style

--- credentialdigger/client_sqlite.py
+++ credentialdigger/client_sqlite.py
@@ -2,8 +2,8 @@ from sqlite3 import Error, connect
 
 from .client import Client
 from .snippet_similarity import build_embedding_model, \
-                        compute_similarity, \
-                        compute_snippet_embedding
+    compute_similarity, \
+    compute_snippet_embedding
 
 class SqliteClient(Client):
     def __init__(self, path):
@@ -508,10 +508,8 @@ class SqliteClient(Client):
         n_updated_snippets = 0
         for d in discoveries:
             if d['state'] == 'new':
-                snippet_embedding = compute_snippet_embedding(
-                                                             [d['snippet']],
-                                                             model
-                                                             )
+                snippet_embedding = compute_snippet_embedding(d['snippet']],
+                                                              model)
                 similarity = compute_similarity(target_snippet_embedding,
                                                 snippet_embedding)
                 if similarity > threshold:
