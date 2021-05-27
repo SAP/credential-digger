b3375cc93912bc457c745c6fb0a909d77fe8fccf
melisandequentzel <melisande.quentzel@gmail.com>
2021-05-27 09:52:20 +0000

    change variable names

--- credentialdigger/snippet_similarity.py
+++ credentialdigger/snippet_similarity.py
@@ -9,15 +9,8 @@ logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 
 def build_embedding_model():
-    bert_model_name = 'small_bert/bert_en_uncased_L-2_H-128_A-2'
-    map_name_to_handle = {'small_bert/bert_en_uncased_L-2_H-128_A-2':
-                          'https://tfhub.dev/tensorflow/small_bert/'
-                          'bert_en_uncased_L-2_H-128_A-2/1'}
-    map_model_to_preprocess = {'small_bert/bert_en_uncased_L-2_H-128_A-2':
-                               'https://tfhub.dev/tensorflow/'
-                               'bert_en_uncased_preprocess/3'}
-    tfhub_handle_encoder = map_name_to_handle[bert_model_name]
-    tfhub_handle_preprocess = map_model_to_preprocess[bert_model_name]
+    tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+    tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-2_H-128_A-2/1'
 
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
     preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess,
@@ -31,8 +24,8 @@ def build_embedding_model():
 
 
 def compute_snippet_embedding(snippet, model):
-    bert_raw_result = tf.squeeze(model(tf.constant(snippet)))
-    small_bert_embeddings = bert_raw_result.numpy()
+    small_bert_result = tf.squeeze(model(tf.constant(snippet)))
+    small_bert_embeddings = small_bert_result.numpy()
     snippet_embedding = np.mean(small_bert_embeddings, axis=0)
     return snippet_embedding
 
