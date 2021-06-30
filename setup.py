fca714e7ecc4174b7ddb9cca0db8ba88118336ac
Marco Rosa <marco.rosa@sap.com>
2021-06-30 17:01:49 +0200

    increment version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 setuptools.setup(
     name='credentialdigger',
-    version='3.3.1',
+    version='3.3.2',
     author='SAP SE',
     maintainer='Marco Rosa, Slim Trabelsi',
     maintainer_email='marco.rosa@sap.com, slim.trabelsi@sap.com',
