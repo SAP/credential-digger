8bcf4993d1b4ca21878409de77eb215114bd7b8d
Marco Rosa <marco.rosa@sap.com>
2021-07-28 17:52:09 +0200

    remove unused imports

--- credentialdigger/client_postgres.py
+++ credentialdigger/client_postgres.py
@@ -1,8 +1,6 @@
 from psycopg2 import Error, connect, extras
 
 from .client import Client
-from .snippet_similarity import (build_embedding_model,
-                                 compute_snippet_embedding)
 
 
 class PgClient(Client):
