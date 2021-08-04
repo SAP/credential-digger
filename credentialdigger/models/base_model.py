462f7262873c4395ad58130996e8d79ee3f9f7f7
melisandequentzel <melisande.quentzel@gmail.com>
2021-08-04 12:32:03 +0000

    add docstrings snippet + base models

--- credentialdigger/models/base_model.py
+++ credentialdigger/models/base_model.py
@@ -5,11 +5,21 @@ from transformers import TFRobertaForSequenceClassification, RobertaTokenizer
 
 class BaseModel(ABC):
 
-    def __init__(self, model, tokenizer):
+    def __init__(self, model, tokenizer, use_auth_token=None):
+        """
+        Parameters
+        ----------
+        model: str
+            the model path
+        tokenizer: str
+            The tokenizer path
+        use_auth_token: str, optional
+            The token to access the model on the Hugging Face hub
+        """
         self.model = TFRobertaForSequenceClassification.from_pretrained(
             model,
             num_labels=2,
-            use_auth_token='api_uAUHSJqiZfkfCjBqnoMkUrWGEIsKJcRliN')
+            use_auth_token=use_auth_token)
         self.tokenizer = RobertaTokenizer.from_pretrained(tokenizer)
 
     @abstractmethod
