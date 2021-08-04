2e9a82d95489a828afc5edeb55d6c3d27be2c5a5
melisandequentzel <melisande.quentzel@gmail.com>
2021-08-04 12:32:03 +0000

    add docstrings snippet + base models

--- credentialdigger/models/base_model.py
+++ credentialdigger/models/base_model.py
@@ -3,18 +3,6 @@ from abc import ABC, abstractmethod
 
 class BaseModel(ABC):
 
-    def __init__(self):
-        """
-        Parameters
-        ----------
-        model: str
-            the model path
-        tokenizer: str
-            The tokenizer path
-        use_auth_token: str, optional
-            The token to access the model on the Hugging Face hub
-        """
-
     @abstractmethod
     def analyze(self, **kwargs):
         pass
