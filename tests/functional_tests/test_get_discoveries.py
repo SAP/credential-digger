3c2f66ec0db17a41430c203b9353b8d88e6732d4
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-05 05:24:04 -0700

    define test class body

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -7,3 +7,6 @@ import pandas as pd
 from credentialdigger.cli import cli
 from credentialdigger.client_sqlite import SqliteClient
 from parameterized import param, parameterize
+
+class TestGetDiscoveries(unittest.TestCase):
+    pass
