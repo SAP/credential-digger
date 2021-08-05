4a0668a80243cd79f581f1bcbb2cb17d4918750f
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-05 05:27:41 -0700

    insert fake discoveries into the database

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -22,3 +22,23 @@ class TestGetDiscoveries(unittest.TestCase):
         client = SqliteClient(cls.db_path)
         client.add_rules_from_file('tests/functional_tests/test_rules.yml')
         client.add_repo('test_repo')
+
+        # Insert fake discoveries
+        discoveries = []
+        discoveries_count = 5
+        for state in ['new', 'false_positive', 'addressing',
+                             'not_relevant', 'fixed']:
+            for i in range(discoveries_count):
+                discovery = {
+
+                    'file_name': 'i',
+                    'commit_id': 'new',
+                    'line_number': 'i',
+                    'snippet': 'pwd=pwd',
+                    'rule_id': 5,
+                    'state': state,
+                    'timestamp': '2021-08-05T01:13',
+                }
+                discoveries.append(discovery)
+            discoveries_count += 1
+        client.add_discoveries(discoveries, 'test_repo')
