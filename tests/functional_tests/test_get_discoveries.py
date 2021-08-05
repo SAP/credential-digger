3966c98a02bb9f608d995ad276b0a80bd888c338
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-05 05:27:04 -0700

    typo + pep8

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -15,10 +15,10 @@ class TestGetDiscoveries(unittest.TestCase):
     def setUpClass(cls):
         # Set up sqlite database and CSV export temporary paths
         cls.tmp_path = tempfile.mkdtemp()
-        cls.db_path = os.path.join(cls.tmp_path, "test_db.sqlite")
-        cls.csv_path = os.path.join(cls.tmp_path, "test.csv")
+        cls.db_path = os.path.join(cls.tmp_path, 'test_db.sqlite')
+        cls.csv_path = os.path.join(cls.tmp_path, 'test.csv')
 
         # Set up sqlite database
         client = SqliteClient(cls.db_path)
-        client.add_rules_from_file("tests/functional_tests/test_rules.yml")
-        client.add_repo("test_repo
+        client.add_rules_from_file('tests/functional_tests/test_rules.yml')
+        client.add_repo('test_repo')
