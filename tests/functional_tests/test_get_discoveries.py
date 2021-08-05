b0f8f5e8a19a998c6fc5c1b12b04437629fe2676
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-05 05:23:36 -0700

    load credential-digger modules + parameterized

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -3,3 +3,7 @@ import tempfile
 import unittest
 
 import pandas as pd
+
+from credentialdigger.cli import cli
+from credentialdigger.client_sqlite import SqliteClient
+from parameterized import param, parameterize
