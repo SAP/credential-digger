5c7b4e32f7a3a3deac3596c39f91ba9993762e4e
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-05 05:25:38 -0700

    Set up sqlite database and CSV export temporary paths

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -8,5 +8,12 @@ from credentialdigger.cli import cli
 from credentialdigger.client_sqlite import SqliteClient
 from parameterized import param, parameterize
 
+
 class TestGetDiscoveries(unittest.TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(cls):
+        # Set up sqlite database and CSV export temporary paths
+        cls.tmp_path = tempfile.mkdtemp()
+        cls.db_path = os.path.join(cls.tmp_path, "test_db.sqlite")
+        cls.csv_path = os.path.join(cls.tmp_path, "test.csv")
