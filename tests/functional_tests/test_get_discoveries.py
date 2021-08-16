92624b264f59fcce8bbdc8e27ad40607ba9a970f
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-16 06:11:02 -0700

    removed extra line

--- tests/functional_tests/test_get_discoveries.py
+++ tests/functional_tests/test_get_discoveries.py
@@ -3,7 +3,6 @@ import tempfile
 import unittest
 
 import pandas as pd
-
 from credentialdigger.cli import cli
 from credentialdigger.client_sqlite import SqliteClient
 from parameterized import param, parameterized
