582fade0d35f18d7878dba8a95c2972390243c66
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-30 08:24:41 -0700

    unselect categories that have 0 discoveries

--- ui/res/js/exportForm.js
+++ ui/res/js/exportForm.js
@@ -2,7 +2,10 @@ function changeAll() {
     let checkBoxes = document.getElementsByName('check');
     for (let i = 0; i < checkBoxes.length; i++) {
         const cb = checkBoxes[i];
-        cb.checked = !$('#cbAll').prop('checked');
+        a_field_id = cb.nextSibling.nextSibling.id;
+        a_field_value = document.getElementById(a_field_id).innerHTML;
+        if (a_field_value != "(0)")
+            cb.checked = !$('#cbAll').prop('checked');
     }
 }
 
