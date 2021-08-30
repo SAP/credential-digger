803f05e68a1afb511b65e1c9a6d56e1719ae1ed2
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2021-08-30 08:24:54 -0700

    format

--- ui/res/js/exportForm.js
+++ ui/res/js/exportForm.js
@@ -1,21 +1,20 @@
 function changeAll() {
-    let checkBoxes = document.getElementsByName('check');
-    for (let i = 0; i < checkBoxes.length; i++) {
-        const cb = checkBoxes[i];
-        a_field_id = cb.nextSibling.nextSibling.id;
-        a_field_value = document.getElementById(a_field_id).innerHTML;
-        if (a_field_value != "(0)")
-            cb.checked = !$('#cbAll').prop('checked');
-    }
+  let checkBoxes = document.getElementsByName("check");
+  for (let i = 0; i < checkBoxes.length; i++) {
+    const cb = checkBoxes[i];
+    a_field_id = cb.nextSibling.nextSibling.id;
+    a_field_value = document.getElementById(a_field_id).innerHTML;
+    if (a_field_value != "(0)") cb.checked = !$("#cbAll").prop("checked");
+  }
 }
 
 function alternateCheckBoxes() {
-    let checkBoxes = document.getElementsByName('check');
-    for (let i = 0; i < checkBoxes.length; i++) {
-        let cb = checkBoxes[i];
-        if (cb.checked) {
-            $('#cbAll').prop('checked', false);
-            return;
-        }
+  let checkBoxes = document.getElementsByName("check");
+  for (let i = 0; i < checkBoxes.length; i++) {
+    let cb = checkBoxes[i];
+    if (cb.checked) {
+      $("#cbAll").prop("checked", false);
+      return;
     }
+  }
 }
