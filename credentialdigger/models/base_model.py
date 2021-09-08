6e8bdddabf785ba6279f080de19859d36b178e10
melisandequentzel <melisande.quentzel@gmail.com>
2021-09-08 11:29:00 +0000

    change snippet + similarity to global variables

--- credentialdigger/models/base_model.py
+++ credentialdigger/models/base_model.py
@@ -16,11 +16,21 @@ class BaseModel(ABC):
         use_auth_token: str, optional
             The token to access the model on the Hugging Face hub
         """
-        self.model = TFRobertaForSequenceClassification.from_pretrained(
-            model,
-            num_labels=2,
-            use_auth_token=use_auth_token)
-        self.tokenizer = RobertaTokenizer.from_pretrained(tokenizer)
+        if globals().get('snippet_model'):
+            self.model = globals()['snippet_model']
+        else:
+            self.model = TFRobertaForSequenceClassification.from_pretrained(
+                model,
+                num_labels=2,
+                use_auth_token=use_auth_token)
+            global snippet_model
+            snippet_model = self.model
+        if globals().get('snippet_tokenizer'):
+            self.tokenizer = globals()['snippet_tokenizer']
+        else:
+            self.tokenizer = RobertaTokenizer.from_pretrained(tokenizer)
+            global snippet_tokenizer
+            snippet_tokenizer = self.tokenizer
 
     @abstractmethod
     def analyze(self, **kwargs):
