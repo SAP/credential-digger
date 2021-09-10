acbb725d8ffc0900f02b8250b36b646bbef21b26
Marco Rosa <marco.rosa@sap.com>
2021-09-10 11:56:20 +0200

    leave only abstract methods

--- credentialdigger/models/base_model.py
+++ credentialdigger/models/base_model.py
@@ -1,11 +1,9 @@
 from abc import ABC, abstractmethod
 
-from transformers import TFRobertaForSequenceClassification, RobertaTokenizer
-
 
 class BaseModel(ABC):
 
-    def __init__(self, model, tokenizer, use_auth_token=None):
+    def __init__(self):
         """
         Parameters
         ----------
@@ -16,22 +14,11 @@ class BaseModel(ABC):
         use_auth_token: str, optional
             The token to access the model on the Hugging Face hub
         """
-        if globals().get('snippet_model'):
-            self.model = globals()['snippet_model']
-        else:
-            self.model = TFRobertaForSequenceClassification.from_pretrained(
-                model,
-                num_labels=2,
-                use_auth_token=use_auth_token)
-            global snippet_model
-            snippet_model = self.model
-        if globals().get('snippet_tokenizer'):
-            self.tokenizer = globals()['snippet_tokenizer']
-        else:
-            self.tokenizer = RobertaTokenizer.from_pretrained(tokenizer)
-            global snippet_tokenizer
-            snippet_tokenizer = self.tokenizer
 
     @abstractmethod
     def analyze(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def analyze_batch(self, **kwargs):
         pass
