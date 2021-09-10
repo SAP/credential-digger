4974e0fd1953521e0f213fd916537b4af0c61451
Marco Rosa <marco.rosa@sap.com>
2021-09-10 15:32:02 +0200

    update with batch vs no-batch processing

--- credentialdigger/models/snippet_model/snippet_model.py
+++ credentialdigger/models/snippet_model/snippet_model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from transformers import TFRobertaForSequenceClassification, RobertaTokenizer
+
 from ..base_model import BaseModel
 
 
@@ -8,22 +10,65 @@ class SnippetModel(BaseModel):
                  model='melisande1/pw1',
                  tokenizer='microsoft/codebert-base-mlm',
                  use_auth_token='api_uAUHSJqiZfkfCjBqnoMkUrWGEIsKJcRliN'):
-        """ This class classifies discoveries as false positives
-        according to snippets.
-
+        """
         Parameters
         ----------
         model: str
+            the model path
             The transformer model's path
         tokenizer: str
-            The tokenizer's path
+            The tokenizer path
         use_auth_token: str, optional
-            The token to access and download the model from
-            the Hugging Face hub
+            The token to access and download the model on the Hugging Face hub
         """
-        super().__init__(model, tokenizer, use_auth_token)
+        if globals().get('snippet_model'):
+            self.model = globals()['snippet_model']
+        else:
+            self.model = TFRobertaForSequenceClassification.from_pretrained(
+                model,
+                num_labels=2,
+                use_auth_token=use_auth_token)
+            global snippet_model
+            snippet_model = self.model
+        if globals().get('snippet_tokenizer'):
+            self.tokenizer = globals()['snippet_tokenizer']
+        else:
+            self.tokenizer = RobertaTokenizer.from_pretrained(tokenizer)
+            global snippet_tokenizer
+            snippet_tokenizer = self.tokenizer
 
-    def analyze(self, discoveries):
+    def analyze_batch(self, discoveries):
+        """ Analyze a snippet and predict whether it is a leak or not.
+        Change each discovery state in-place.
+
+        Parameters
+        ----------
+        discoveries: list of dict
+            The discoveries to classify
+
+        Returns
+        -------
+        discoveries: list of dict
+            The discoveries, with states updated according to
+            the model's predictions
+        """
+        # We have to classify only the "new" discoveries
+        new_discoveries = [d for d in discoveries if d['state'] == 'new']
+        no_new_discoveries = [d for d in discoveries if d['state'] != 'new']
+        # Create a dataset with all the preprocessed (new) snippets
+        data = self._pre_process([d['snippet'] for d in new_discoveries])
+        # data = self._preprocess_batch_data(snippets)
+        # Compute a prediction for each snippet
+        outputs = self.model.predict(data)
+        logits = outputs['logits']
+        predictions = tf.argmax(logits, 1)
+        # Check predictions and set FP discoveries accordingly
+        for d, p in zip(new_discoveries, predictions):
+            if p == 0:
+                d['state'] = 'false_positive'
+        return new_discoveries + no_new_discoveries
+
+    def analyze(self, discovery):
         """ Analyze a snippet and predict whether it is a leak or not.
 
         Parameters
@@ -39,41 +84,37 @@ class SnippetModel(BaseModel):
         n_false_positives: int
             The number of false positives detected by the model
         """
-        fp_discoveries = [d for d in discoveries
-                                    if d['state'] == 'false_positive']
-        new_discoveries = [d for d in discoveries
-                              if d['state'] != 'false_positive']
-        snippets = [d['snippet'] for d in new_discoveries]
-        data = self.preprocess_data(snippets)
+        # Preprocess the snippet
+        data = self._pre_process(discovery['snippet'])
+        # Classify the processed snippet
         outputs = self.model.predict(data)
-        logits = outputs['logits']
-        predictions = tf.argmax(logits, 1)
-        n_false_positives = 0
-        for d, p in zip(new_discoveries, predictions):
-            if p == 0:
-                d['state'] = 'false_positive'
-                n_false_positives += 1
-        discoveries = fp_discoveries + new_discoveries
-        return discoveries, n_false_positives
+        predictions = tf.argmax(outputs['logits'], 1)
+        if predictions[0] == 0:
+            # The model classified this snippet as a false positive
+            # (i.e., spam)
+            return True
 
-    def preprocess_data(self, snippets):
+    def _pre_process(self, snippet):
         """ Compute encodings of snippets and format them to a standard
         Tensorflow dataset.
 
         Parameters
         ----------
-        snippets: list
-            The snippets to be preprocessed
+        snippet: str
+            The snippet to be preprocessed
 
         Returns
         -------
         tf.data.Dataset
             The dataset to be fed to the classifier
         """
-        encodings = self.tokenizer(snippets,
+        # In our model, encodings and features are the same
+        # So, we don't have to compute features and we can directly use
+        # encodings to create the dataset
+        encodings = self.tokenizer(snippet,
                                    truncation=True,
                                    padding=True)
-        features = {x: encodings[x]
-                    for x in self.tokenizer.model_input_names}
-        dataset = tf.data.Dataset.from_tensor_slices((features)).batch(8)
-        return dataset
+        # Transform into a tf dataset
+        # Please note that the encodings must be cast to dict in order to avoid
+        # a ValueError at the model prediction
+        return tf.data.Dataset.from_tensor_slices((dict(encodings))).batch(8)
