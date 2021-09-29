98ad97ee51d3c5ef07e8d501d9415720fa62303f
melisandequentzel <melisande.quentzel@gmail.com>
2021-09-29 14:06:01 +0000

    fix typo

--- ui/res/js/discoveries.js
+++ ui/res/js/discoveries.js
@@ -238,7 +238,8 @@ function initDiscoveriesDataTable() {
               `
 	  <input type="checkbox" class="cbSim" id="cbSim" value="yes" checked>
           <label class="cb-label">Update similar discoveries</label>`;
-	    var color_box = `${item.color} ? `
+	    
+	    var color_box = `${item.color ? `
 	    <div style="background-color:hsl("+${item.color}+", 100%, 50%);padding:20px;width:20px"></div>`
 	    :`<div style="background-color:hsl(20, 100%, 50%);padding:20px;width:20px"></div>`}`;
 
