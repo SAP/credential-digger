d12708e7d23c96f9a63ea6f5cb79ec735f8f9cdd
melisandequentzel <melisande.quentzel@gmail.com>
2021-09-29 13:58:09 +0000

    remove test text from div

--- ui/res/js/discoveries.js
+++ ui/res/js/discoveries.js
@@ -238,9 +238,8 @@ function initDiscoveriesDataTable() {
               `
 	  <input type="checkbox" class="cbSim" id="cbSim" value="yes" checked>
           <label class="cb-label">Update similar discoveries</label>`;
-           var hue_color = ${item.color} 
-	    var color_box = `hue_color ? `
-	    <div style="background-color:hsl("+hue_color+", 100%, 50%);padding:20px;width:20px">test</div>`
+	    var color_box = `${item.color} ? `
+	    <div style="background-color:hsl("+${item.color}+", 100%, 50%);padding:20px;width:20px"></div>`
 	    :`<div style="background-color:hsl(20, 100%, 50%);padding:20px;width:20px"></div>`}`;
 
             return {
