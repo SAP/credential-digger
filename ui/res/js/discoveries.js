01ee51f95070a67616ed89d1428b3992274be306
melisandequentzel <melisande.quentzel@gmail.com>
2021-09-29 10:32:25 +0000

    test html color box

--- ui/res/js/discoveries.js
+++ ui/res/js/discoveries.js
@@ -241,14 +241,16 @@ function initDiscoveriesDataTable() {
             
 	    //var color_box = 
 	    //<div style="background-color:hsl("+${item.hue}+", 100%, 50%)">""</div>
+	    var color_box = 
+	    <div style="background-color:hsl(20, 100%, 50%)">""</div>
 	    //color_box = <div class='color-box' style="background-color:hsl(0, 100%, 50%)></div>
-	    var color_box = document.createElement('div');
-	    color_box.innerHTML = "";
-	    color_box.style.padding = 20px;
+	    //var color_box = document.createElement('div');
+	    //color_box.innerHTML = "";
+	    //color_box.style.padding = 20px;
 	    //if ${item.hue}:
             //    color_box.style.backgroundColor = "hsl("+${item.hue}+", 100%, 50%)";
 	    //else:
-            color_box.style.backgroundColor = "hsl(0, 100%,100%)";
+            //color_box.style.backgroundColor = "hsl(0, 100%,100%)";
 
             return {
               ...item,
