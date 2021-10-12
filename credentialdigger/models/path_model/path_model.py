5f8abfea149f1a9132801e3e8f122e5c5ca4fa84
Marco Rosa <marco.rosa@sap.com>
2021-10-12 16:03:12 +0200

    fix pep8

--- credentialdigger/models/path_model/path_model.py
+++ credentialdigger/models/path_model/path_model.py
@@ -7,7 +7,8 @@ class PathModel(BaseModel):
 
     def __init__(self):
         self.fp_keywords = re.compile(
-            r'^(con)?test|example|demo^(cra)?^(graph)?|package-lock|makefile|gruntfile|\.md$|css$|\.rst$')
+            r'(con)?test|example|demo^(cra)?^(graph)?|package-lock|makefile|'
+            'gruntfile|\.md$|css$|\.rst$')
 
     def analyze(self, discovery):
         """ Analyze a path and predict whether it is a false positive or not.
