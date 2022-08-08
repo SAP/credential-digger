2657884bc2d1d341cc12fdf896b5c37497705211
Marco Rosa <marco.rosa@sap.com>
2022-08-08 11:31:46 +0200

    upgrade package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements():
 
 setuptools.setup(
     name='credentialdigger',
-    version='4.8.3',
+    version='4.9.0',
     author='SAP SE',
     maintainer='Marco Rosa, Slim Trabelsi',
     maintainer_email='marco.rosa@sap.com, slim.trabelsi@sap.com',
