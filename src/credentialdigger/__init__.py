3f6fe8efd6a2c825b0e477f7d8110b6fced06945
Marco Rosa <marco.rosa@sap.com>
2024-01-25 16:48:10 +0100

    Get version at runtime

--- src/credentialdigger/__init__.py
+++ src/credentialdigger/__init__.py
@@ -1,3 +1,8 @@
+import importlib.metadata
+
 from .client import Client
 from .client_postgres import PgClient
 from .client_sqlite import SqliteClient
+
+
+__version__ = importlib.metadata.version('credentialdigger')
